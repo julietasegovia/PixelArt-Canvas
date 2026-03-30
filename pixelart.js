1020b5b916b782dce61df9e3d987f8760cd0345c
julietasegovia <julietavsegovia@gmail.com>
2026-03-30 13:23:42 -0300

    Canvas creation corrected

--- pixelart.js
+++ pixelart.js
@@ -6,13 +6,13 @@ let isDragging = false;
 let isErasing = false;
 let cells = [];
 
-for(let row=0; row < 20; row++){
+for(let row = 0; row < 20; row++){
     for(let col = 0; col < 20; col++){
         const cell = document.createElement('div');
         cell.classList.add('pixel');
         cell.dataset.row = row;
         cell.dataset.col = col;
         grid.appendChild(cell);
-        cell.push(cell);
+        cells.push(cell);
     }
 }
